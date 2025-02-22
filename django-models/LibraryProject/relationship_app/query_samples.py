e5405ea841ff61bf8695361d1ffd48bf95127081
Yaw Mintah <yawmintah1@gmail.com>
2025-02-22 17:29:04 +0000

    fixed checker error: relationship_app/query_samples.py doesn't contain: ['Library.objects.get(name=library_name)']

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,23 +1,7 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-# # Create an author
-# author = Author.objects.create(name='J.K. Rowling')
-
-# # Create a book
-# book = Book.objects.create(title='Harry Potter and the Philosopher\'s Stone', author=author)
-
-# # Create a library
-# library = Library.objects.create(name='Sam Jonah Library')
-
-# # Add the book to the library
-# library.books.add(book)
-
-# # Create a librarian
-# librarian = Librarian.objects.create(name='Joel', library=library)
-
-
 # List all bookd in a library
-library = Library.objects.get(name=Sam)
+library = Library.objects.get(name='Sam Jonah Library')
 books_in_library = library.books.all()
 
 # Query all books by a specific author
