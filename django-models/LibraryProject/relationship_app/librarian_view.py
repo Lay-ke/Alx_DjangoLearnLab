58244345ccd921026eec9b14e0d8457376eb4138
Yaw Mintah <yawmintah1@gmail.com>
2025-02-23 13:15:26 +0000

    fixed checker error: separate the views for authentication

--- django-models/LibraryProject/relationship_app/librarian_view.py
+++ django-models/LibraryProject/relationship_app/librarian_view.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test, login_required
 
 def is_librarian(user):
     return user.groups.filter(name='Librarian').exists()
 
+
 @login_required
 @user_passes_test(is_librarian)
 def librarian_view(request):
-    return HttpResponse("Welcome, Librarian!")
+    return render(request, 'relationship_app/librarian_view.html')
--- django-models/LibraryProject/relationship_app/member_view.py
+++ django-models/LibraryProject/relationship_app/member_view.py
@@ -1,11 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test, login_required
 
 
 def is_member(user):
     return user.groups.filter(name='Member').exists()
 
+
 @login_required
 @user_passes_test(is_member)
 def member_view(request):
-    return HttpResponse("Welcome, Member!")
+    return render(request, 'relationship_app/member_view.html')
