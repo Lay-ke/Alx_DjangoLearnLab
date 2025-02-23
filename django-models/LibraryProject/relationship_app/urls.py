3e939590ddbcae59768e43cf096b25b75f366333
Yaw Mintah <yawmintah1@gmail.com>
2025-02-23 11:41:01 +0000

    fixed checker error: wrong import views structure

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -4,8 +4,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
-    path('login/', LoginView.as_view(template_name = 'relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name = 'relationship_app/logout.html'), name='logout'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('books/', views.list_books, name='books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library-detail')
