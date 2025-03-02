9406b27b8a0e28fb3b16d6ee08ab268498c3c3c0
Yaw Mintah <yawmintah1@gmail.com>
2025-03-02 17:36:04 +0000

    fix: creation of exampleForm in bookshelf/ forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponseForbidden, HttpResponse
 from .models import Book
-from .forms import BookForm  # Assuming you have a BookForm for creating/editing books
+from .forms import ExampleForm  # Assuming you have an ExampleForm for creating/editing books
 
 def add_csp_header(response):
     response['Content-Security-Policy'] = "default-src 'self'; script-src 'self'; style-src 'self';"
@@ -19,12 +19,12 @@ def view_book(request, book_id):
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     if request.method == 'POST':
-        form = BookForm(request.POST)
+        form = ExampleForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')  # Redirect to the list of books after creation
     else:
-        form = BookForm()
+        form = ExampleForm()
     response = render(request, 'bookshelf/book_form.html', {'form': form})
     return add_csp_header(response)
 
@@ -33,12 +33,12 @@ def create_book(request):
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
-        form = BookForm(request.POST, instance=book)
+        form = ExampleForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
             return redirect('book_detail', book_id=book.id)
     else:
-        form = BookForm(instance=book)
+        form = ExampleForm(instance=book)
     response = render(request, 'bookshelf/book_form.html', {'form': form})
     return add_csp_header(response)
 
