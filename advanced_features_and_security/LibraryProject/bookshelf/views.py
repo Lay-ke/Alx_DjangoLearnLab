64be15d43431f5d642c97df346e502ff4e1f746a
Yaw Mintah <yawmintah1@gmail.com>
2025-03-02 17:40:42 +0000

    fix: creation of form_example.html

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponseForbidden, HttpResponse
 from .models import Book
-from .forms import ExampleForm  # Assuming you have an ExampleForm for creating/editing books
+from .forms import BookForm, ExampleForm  # Import ExampleForm
 
 def add_csp_header(response):
     response['Content-Security-Policy'] = "default-src 'self'; script-src 'self'; style-src 'self';"
@@ -19,12 +19,12 @@ def view_book(request, book_id):
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
     if request.method == 'POST':
-        form = ExampleForm(request.POST)
+        form = BookForm(request.POST)
         if form.is_valid():
             form.save()
             return redirect('book_list')  # Redirect to the list of books after creation
     else:
-        form = ExampleForm()
+        form = BookForm()
     response = render(request, 'bookshelf/book_form.html', {'form': form})
     return add_csp_header(response)
 
@@ -33,12 +33,12 @@ def create_book(request):
 def edit_book(request, book_id):
     book = get_object_or_404(Book, id=book_id)
     if request.method == 'POST':
-        form = ExampleForm(request.POST, instance=book)
+        form = BookForm(request.POST, instance=book)
         if form.is_valid():
             form.save()
             return redirect('book_detail', book_id=book.id)
     else:
-        form = ExampleForm(instance=book)
+        form = BookForm(instance=book)
     response = render(request, 'bookshelf/book_form.html', {'form': form})
     return add_csp_header(response)
 
@@ -56,4 +56,16 @@ def delete_book(request, book_id):
 def book_list(request):
     books = Book.objects.all()
     response = render(request, 'bookshelf/book_list.html', {'books': books})
+    return add_csp_header(response)
+
+# New view for handling ExampleForm
+def example_form_view(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('success_page')  # Redirect to a success page after form submission
+    else:
+        form = ExampleForm()
+    response = render(request, 'bookshelf/form_example.html', {'form': form})
     return add_csp_header(response)
