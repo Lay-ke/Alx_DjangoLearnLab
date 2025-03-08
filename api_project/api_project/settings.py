453e9509af4115b5fc6eee36c4e790e7fdea39b2
Yaw Mintah <yawmintah1@gmail.com>
2025-03-08 15:02:15 +0000

    fix: included rest_framework.authentication.TokenAuthentication to settings

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
 
 DEFAULT_AUTHENTICATION_CLASSES = [
     'rest_framework.authentication.BasicAuthentication',
+    'rest_framework.authentication.TokenAuthentication',
     'rest_framework.authentication.SessionAuthentication',
 ]
 
