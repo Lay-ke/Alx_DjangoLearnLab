daada0966f98f4bbb4c8f24e67305efbb78cfaae
Yaw Mintah <yawmintah1@gmail.com>
2025-03-15 15:50:59 +0000

    fix: api/urls.py doesn't contain: books/update, books/delete

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -3,8 +3,8 @@ from .views import CreateView, DeleteView, UpdateView, DetailView, ListView
 
 urlpatterns = [
     path('books/create/', CreateView.as_view(), name='create'),
-    path('books/<int:pk>/delete/', DeleteView.as_view(), name='delete'),
-    path('books/<int:pk>/update/', UpdateView.as_view(), name='update'),
+    path('books/delete/<int:pk>/', DeleteView.as_view(), name='delete'),
+    path('books/update/<int:pk>/', UpdateView.as_view(), name='update'),
     path('books/<int:pk>/', DetailView.as_view(), name='detail'),
     path('books/', ListView.as_view(), name='list'),
 ]
