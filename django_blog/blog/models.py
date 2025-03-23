93bcc7c9cf54eb1b6548f2f8e3dc0e259c078617
Yaw Mintah <yawmintah1@gmail.com>
2025-03-23 22:09:46 +0000

    fix: doesn't contain: [Post.objects.filter]

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -12,6 +12,14 @@ class Blog(models.Model):
     def __str__(self):
         return self.title
     
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    published_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
 
 class Comment(models.Model):
     post = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name='comments')
@@ -27,7 +35,7 @@ class Comment(models.Model):
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
     posts = models.ManyToManyField(Blog, related_name='tags')
-    
+
     def __str__(self):
         return self.name
 # class User(AbstractUser):
