841e82a32e93159fa4cb28b8815dd545150fe271
Yaw Mintah <yawmintah1@gmail.com>
2025-03-23 12:15:31 +0000

    chore: initial commit for Creating Blog Post Management Features

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
     path('register/', views.register, name='blog-register'),
     path('profile/', views.profile, name='blog-profile'),
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='blog-logout'),
+    path('posts/', views.PostListView.as_view(), name='post-list'),
+    path('posts/new/', views.PostCreateView.as_view(), name='post-create'),
+    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post-update'),
+    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
 ]
