2bf5004457ee042dd9aab9e5d849badcd0361b4c
Yaw Mintah <yawmintah1@gmail.com>
2025-03-29 09:36:30 +0000

    fix: contains generics.get_object_or_404(Post, pk=pk)

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -52,7 +52,7 @@ class FeedView(APIView):
 # Like post
 @login_required
 def like_post(request, pk):
-    post = generics.get_object_or_404(Post, id=pk)
+    post = generics.get_object_or_404(Post, pk=pk)
     like, created = Like.objects.get_or_create(user=request.user, post=post)
     
     if not created:
@@ -71,7 +71,7 @@ def like_post(request, pk):
 # Unlike post
 @login_required
 def unlike_post(request, pk):
-    post = generics.get_object_or_404(Post, id=pk)
+    post = generics.get_object_or_404(Post, pk=pk)
     like = Like.objects.filter(user=request.user, post=post).first()
     
     if not like:
